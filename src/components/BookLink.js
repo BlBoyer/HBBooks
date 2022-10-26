8709edec10ed4df32095e8fe8236307710a4861f
BenB <boyer.benlee@gmail.com>
2022-10-25 21:20:54 -0700

    added lang link hover text

--- src/components/BookLink.js
+++ src/components/BookLink.js
@@ -1,5 +1,12 @@
 export default function BookLink({book})
 {
+    const langs = {
+    "eng":"English",
+    "deu":"Deutsche",
+    "esp":"Español",
+    "ukr":"Українська",
+    "zho":"Zhōng Wén"
+}
     const nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"];
     var bookName = book.name.slice(0,1).toUpperCase() + book.name.slice(1,5) + ' ' + nums.indexOf(book.name.slice(5).toLowerCase());
     return (
@@ -10,7 +17,7 @@ export default function BookLink({book})
                     {
                         if(Object.entries(urlObj)[0][1].length > 0)
                         {
-                            return <a href={Object.entries(urlObj)[0][1]} className="mx-1" key={ind}>{Object.entries(urlObj)[0][0]}</a>;
+                            return <a href={Object.entries(urlObj)[0][1]} title={langs[Object.entries(urlObj)[0][0]]} className="mx-1" key={ind}>{Object.entries(urlObj)[0][0]}</a>;
                         } else 
                         {
                             return <a href="" className="mx-1 nav-item pe-none" key={ind}>&#199;S!</a>
