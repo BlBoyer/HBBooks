9701d95a38bbb65701aa10708e62633ff25c03aa
BenB <boyer.benlee@gmail.com>
2023-01-31 10:03:11 -0800

    added book eight and eng link, dusted carousel component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,42 +6,39 @@ const img4 = require('../img/monthFour.jpg');
 const img5 = require('../img/monthFive.jpg');
 const img6 = require('../img/monthSix.jpg');
 const img7 = require('../img/monthSeven.jpg');
+const img8 = require('../img/monthEight.jpg');
 //give an options parameter for carousel options, like cycle, arrow, etc.
 export default function Carousel({identifier})
 {
-    const images=[img1,img2,img3, img4, img5, img6, img7];
+    const images=[img1,img2,img3, img4, img5, img6, img7, img8];
     //test images is array of images
     var [active, setActive] = useState(0);
     var [count, setCount] = useState(0);
-    //var [started, setStarted] = useState(false);
-    //we're going to translate the old pic out as it fades out, and fade the new one in or vice versa.
-/*     useEffect(()=>{
-        document.getElementById(identifier).addEventListener('animationstart', setStarted(true));
-    },[]); */
-    useEffect(()=>
+
+    useEffect(() =>
     {
-        //if (started)
-        //{
-            var interval=setInterval(()=>
-            {
-                setCount(count=>count+1);
-            }, 4000);
-        //}
+        var interval=setInterval(()=>
+        {
+            setCount(count=>count+1);
+        }, 4000);
+
         return ()=>clearInterval(interval);
     },[]);
-    useEffect(()=>{
-        //this is switching too early on the first two iterations on slow connections
+
+    useEffect(() => 
+    {
         if (count > 0)
         {
-            if (active==images.length-1)
+            if (active == images.length-1)
             {
                 setActive(0);
             } else 
             {
-                setActive(active=> active+=1);
+                setActive(active => active += 1);
             }
         }
     }, [count]);
+
     return (
         <div className="row justify-content-center myCarousel mt-3 mb-5">
             <div className="col slider-back">
