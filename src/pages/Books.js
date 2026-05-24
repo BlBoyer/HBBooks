4e228a58aebe7dbb24c5095b4bbd75585546fd40
bboy <boyer.benlee@gmail.com>
2026-05-23 19:32:54 -0700

    translation notes

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -13,7 +13,16 @@ export default function Books({capsule})
             <React.Suspense fallback={<Loading />} >
                 <BookCarousel identifier="disp1" />
             </React.Suspense>
-            <div className="row justify-content-center mb-2 main text">
+            <div className="d-flex flex-row justify-content-center align-items-center w-100 mb-3">
+                <span className='me-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="maroon" class="icon icon-tabler icons-tabler-filled icon-tabler-alert-square-rounded">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M12 2l.642 .005l.616 .017l.299 .013l.579 .034l.553 .046c4.687 .455 6.65 2.333 7.166 6.906l.03 .29l.046 .553l.041 .727l.006 .15l.017 .617l.005 .642l-.005 .642l-.017 .616l-.013 .299l-.034 .579l-.046 .553c-.455 4.687 -2.333 6.65 -6.906 7.166l-.29 .03l-.553 .046l-.727 .041l-.15 .006l-.617 .017l-.642 .005l-.642 -.005l-.616 -.017l-.299 -.013l-.579 -.034l-.553 -.046c-4.687 -.455 -6.65 -2.333 -7.166 -6.906l-.03 -.29l-.046 -.553l-.041 -.727l-.006 -.15l-.017 -.617l-.004 -.318v-.648l.004 -.318l.017 -.616l.013 -.299l.034 -.579l.046 -.553c.455 -4.687 2.333 -6.65 6.906 -7.166l.29 -.03l.553 -.046l.727 -.041l.15 -.006l.617 -.017c.21 -.003 .424 -.005 .642 -.005zm.01 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" />
+                    </svg>
+                </span>
+                <span><em>Russian and Ukranian translations are currently being reworked.</em></span>
+            </div>
+            <div className="row justify-content-center my-2 main text">
                 <p className="col d-inline-flex over-text rounded">
                     The Hatty Barty series has been translated to various languages. You can find copies of the books for sale on Amazon, using the links below:
                 </p>
